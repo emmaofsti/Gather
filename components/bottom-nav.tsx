b6b98154bc6e71fce1f0f93599381c51a90489a0
Emma Øfsti <emmaofsti@Mac.localdomain>
2026-04-09 18:22:16 +0200

    Rename nav Turer to Gatherings

--- components/bottom-nav.tsx
+++ components/bottom-nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const items = [
-  { href: "/", label: "Turer", icon: "🧭" },
+  { href: "/", label: "Gatherings", icon: "🧭" },
   { href: "/trips/new", label: "Ny", icon: "✦" },
   { href: "/me", label: "Meg", icon: "☻" },
 ];
